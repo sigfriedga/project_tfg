e36a5d65322c8a833d29a790c94939456df3cadb
sigfriedga <sigfriedga@hotmail.com>
2021-05-18 20:30:38 +0200

    team area mediaQ for 768

---
+++ includes/header/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>SIWI AGENCY</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link
+    href="https://fonts.googleapis.com/css2? family = Oswald: wght @ 300; 500; 700 & family = Source + Sans + Pro: wght @ 300; 400; 700 & display = swap "
+    rel="stylesheet">
+  <script src="https://kit.fontawesome.com/6cf2391fde.js" crossorigin="anonymous"></script>
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+<body>
+  <div class="img__nav">
+    <img src="img/logo2.png" alt="">
+  </div>
+  <div class="hamburger">
+    <div class="_layer -top"></div>
+    <div class="_layer -mid"></div>
+    <div class="_layer -bottom"></div>
+  </div>
+  <nav class="menuppal nav__btn nav__mb">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="team.html">Team</a></li>
+      <li><a href="#" class="btn__orange">Contact</a></li>
+    </ul>
+  </nav>
+  <!--nav__btn-->
