8820a1a6a5beaaee9f3230b9dabc504a4b52918d
sigfriedga <sigfriedga@hotmail.com>
2021-06-03 16:57:21 +0200

    if exist email with php

--- action_page.php
+++ action_page.php
@@ -1,4 +1,4 @@
-<?php include("includes/bd/bd.php"); 
+<?php include("includes/connect/bd.php"); 
 
 if(isset($_POST['checkin'])){
   $name = $_POST["name"];
@@ -15,6 +15,37 @@ if(isset($_POST['checkin'])){
     if(!$runInsert){
       echo " Error in SQL query";
     }
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+      $email = $_POST["email"];
+
+      $searchemail = "SELECT email from contactform WHERE email='$email'";
+
+      //Realizamos la consulta y anadimos $connect, ya que es la variable que creamos en nuestro archivo db.php
+      $result = $connect->query($searchemail);
+
+      //Usaremos la funcion mysqli_num_rows en la consulta $result,
+      //esta funcion nos regresa el numero de filas en el resultado
+      $counter = mysqli_num_rows($result);
+
+      //SI SI EXISTE una fila, quiere decir QUE SI ESTA EL CORREO EN LA BASE DE DATOS
+      if($counter == 1) {
+         echo 'This Email already exist';
+      } 
+ }
+
+
+    
 }
 
 ?>
+<link rel="stylesheet" href="css/main.css">
+<section class="confirmed">
+<div class="confirmed__area">
+  <img src="img/logo.png" alt="">
+  <h1>¡Thanks for contact us!</h1>
+  <p>Thanks again for joining us! Your first subscription email will be arriving soon</p>
+  <a href="index.php"><span class="btn__orange">Come Back</span></a>
+  </div>
+</section>
--- team.php
+++ team.php
@@ -1,80 +1,95 @@
 <?php include("includes/header/header.php"); ?>
 <?php include("includes/bd/bd.php"); ?>
 
-  <header class="header__area">
-    <div class="header__text">
-      <h1>Custom Software and SEO Development</h1>
-      <p>From concept to code and design to deployment, we develop reliable and easy-to-use customized software
-        products.</p>
-      <div class="header__btn">
-        <a class="btn__orange" href="http://">Start Now</a>
-      </div>
-      <!--header__btn-->
+<header class="header__area">
+  <div class="header__text">
+    <h1>Custom Software and SEO Development</h1>
+    <p>From concept to code and design to deployment, we develop reliable and easy-to-use customized software
+      products.</p>
+    <div class="header__btn">
+      <a class="btn__orange" href="http://">Start Now</a>
     </div>
-    <!--header__text-->
-    <div class="header__img">
-      <img src="img/Team work-bro.svg" alt="">
-    </div>
+    <!--header__btn-->
+  </div>
+  <!--header__text-->
+  <div class="header__img">
+    <img src="img/Team work-bro.svg" alt="">
+  </div>
 
-  </header>
+</header>
 
-  <section class="team">
-    <div class="team__area slide-track">
-      <h2>Our Expert Team Members</h2>
-      <p>This new-found knowledge may then be used by engineers to create new tools and machines</p>
-      <div class="team__boxes">
-        <div class="team__box slide">
-          <div class="team__img">
-            <img src="img/sga.jpg" alt="">
-            <div class="team__text">
-              <h3>Sigfried</h3>
-              <p>Web Developer</p>
-              <div class="team__icons">
-                <a href="https://github.com/sigfriedga"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/sigfriedga/"><i class="fab fa-linkedin-in"></i></a>
-                <a href="https://www.facebook.com/sigfriedga"><i class="fab fa-facebook-f"></i></a>
-                <a href="https://twitter.com/sigfriedga"><i class="fab fa-twitter"></i></a>
-              </div>
+<section class="team">
+  <div class="team__area slide-track">
+    <h2>Our Expert Team Members</h2>
+    <p>This new-found knowledge may then be used by engineers to create new tools and machines</p>
+    <div class="team__boxes">
+      <div class="team__box slide">
+        <div class="team__img">
+          <img src="img/sga.jpg" alt="">
+          <div class="team__text">
+            <h3>Sigfried</h3>
+            <p>Web Developer</p>
+            <div class="team__icons">
+              <a href="https://github.com/sigfriedga"><i class="fab fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/sigfriedga/"><i class="fab fa-linkedin-in"></i></a>
+              <a href="https://www.facebook.com/sigfriedga"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://twitter.com/sigfriedga"><i class="fab fa-twitter"></i></a>
             </div>
           </div>
         </div>
-        <!--box-->
+      </div>
+      <!--box-->
 
-        <div class="team__box slide">
-          <div class="team__img">
-            <img src="img/igp.jpeg" alt="">
-            <div class="team__text">
-              <h3>Inma</h3>
-              <p>SEO Manager</p>
-              <div class="team__icons">
-                <a href="https://www.instagram.com/inmagall/"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/inma-gallego-seo/"><i class="fab fa-linkedin-in"></i></a>
-                <a href="https://www.facebook.com/inma.gallego.73"><i class="fab fa-facebook-f"></i></a>
-                <a href="https://twitter.com/gallego_inma"><i class="fab fa-twitter"></i></a>
-              </div>
+      <div class="team__box slide">
+        <div class="team__img">
+          <img src="img/igp.jpeg" alt="">
+          <div class="team__text">
+            <h3>Inma</h3>
+            <p>SEO Manager</p>
+            <div class="team__icons">
+              <a href="https://www.instagram.com/inmagall/"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/inma-gallego-seo/"><i class="fab fa-linkedin-in"></i></a>
+              <a href="https://www.facebook.com/inma.gallego.73"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://twitter.com/gallego_inma"><i class="fab fa-twitter"></i></a>
             </div>
           </div>
         </div>
-        <!--box-->
       </div>
-      <!--boxes-->
+      <!--box-->
     </div>
-  </section>
-
-  <section class="form__area" id="contact">
-    <h2>Contact</h2>
+    <!--boxes-->
+  </div>
+</section>
+<form action="success.html">
     <p>
-      Please contact us if you have questions or comments about this service.</p>
-    <form action="action_page.php" name="form" method="POST">
-      <label for="name">First name</label>
-      <input type="text" id="name" name="name" placeholder="Name...">
-      <label for="suname">Surname</label>
-      <input type="text" id="surname" name="surname" placeholder="Surname..">
-      <label for="mail">Mail</label>
-      <input type="email" id="email" name="email" placeholder="ejemplo@email.com">
-      <input class="btn__orange" type="submit" name="checkin" value="send">
-    </form>
+        Your Email<br>
+        <input name="email">
+    </p>
 
-  </section>
+    
+<script src="https://vivirenremoto.github.io/doomcaptcha/script.js?version=16" countdown="on" label="Captcha" enemies="4"></script>
 
-  <?php include("includes/footer/footer.php"); ?>
+
+    <p>
+        <button>Unsubscribe</button>
+    </p>
+</form>
+
+
+<section class="form__area" id="contact">
+  <h2>Contact</h2>
+  <p>
+    Please contact us if you have questions or comments about this service.</p>
+  <form action="action_page.php" name="form" method="POST">
+    <label for="name">First name</label>
+    <input type="text" id="name" name="name" placeholder="Name..." required>
+    <label for="suname">Surname</label>
+    <input type="text" id="surname" name="surname" placeholder="Surname.." required>
+    <label for="mail">Mail</label>
+    <input type="email" id="email" name="email" placeholder="ejemplo@email.com" required>
+    <input class="btn__orange" type="submit" name="checkin" value="send">
+  </form>
+
+</section>
+
+<?php include("includes/footer/footer.php"); ?>
